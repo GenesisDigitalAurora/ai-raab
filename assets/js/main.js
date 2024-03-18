1210cd6fcfe358baa743ce31074ac48c9cfc7c6b
GeoDistor <geo.distor@gmail.com>
2024-03-18 11:04:55 -0600

    Remove the request to some js files that are useless

--- assets/js/main.js
+++ assets/js/main.js
@@ -392,20 +392,20 @@ $(function () {
 
     //====== Magnific Popup
 
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-        // other options
-    });
+    // $('.video-popup').magnificPopup({
+    //     type: 'iframe'
+    //     // other options
+    // });
 
 
     //===== Magnific Popup
 
-    $('.image-popup').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
+    // $('.image-popup').magnificPopup({
+    //     type: 'image',
+    //     gallery: {
+    //         enabled: true
+    //     }
+    // });
 
 
     //===== niceSelect js
@@ -415,16 +415,16 @@ $(function () {
 
 
 
-    if ($('.wow').length) {
-        var wow = new WOW({
-            boxClass: 'wow', // animated element css class (default is wow)
-            animateClass: 'animated', // animation css class (default is animated)
-            offset: 150, // distance to the element when triggering the animation (default is 0)
-            mobile: true, // trigger animations on mobile devices (default is true)
-            live: true // act on asynchronously loaded content (default is true)
-        });
-        wow.init();
-    }
+    // if ($('.wow').length) {
+    //     var wow = new WOW({
+    //         boxClass: 'wow', // animated element css class (default is wow)
+    //         animateClass: 'animated', // animation css class (default is animated)
+    //         offset: 150, // distance to the element when triggering the animation (default is 0)
+    //         mobile: true, // trigger animations on mobile devices (default is true)
+    //         live: true // act on asynchronously loaded content (default is true)
+    //     });
+    //     wow.init();
+    // }
 
 
 
