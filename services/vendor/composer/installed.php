2c6c47ad6056f349519109eb311bbbd0534e9f11
GenesisDigitalAurora <genesis.digital2920@gmail.com>
2025-09-14 20:28:55 -0600

    Services

---
+++ services/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'geodi/services',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '84f0cd7cc55861aea71f32f2f7c42a1a66b98aa2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'geodi/services' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '84f0cd7cc55861aea71f32f2f7c42a1a66b98aa2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
